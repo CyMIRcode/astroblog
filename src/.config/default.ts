a42a370bbcf8264139634951427dffebc0357384
chris <148228792+CyMIRcode@users.noreply.github.com>
2026-01-22 11:43:13 +0800

    Update default.ts

--- src/.config/default.ts
+++ src/.config/default.ts
@@ -5,8 +5,8 @@ import type { ThemeConfig } from '~/types'
 
 export const defaultConfig: ThemeConfig = {
   site: {
-    title: '站长日志 ',
-    subtitle: 'IDV.ng',
+    title: 'IDV.ng 站长日志 ',
+    subtitle: 'admin.IDV.ng',
     author: 'peter 船长',
     description: 'IDV.ng 站长日志',
     website: 'https://astroblog-lovat.vercel.app/',
